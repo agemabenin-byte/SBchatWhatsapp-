2f7a3e72844d0e9e6106a719bdf02360c7bcdeec
agemabenin-byte <agemabenin@gmail.com>
2026-04-13 17:57:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -278,14 +278,6 @@ async function executeSendPrivate() {
     goBack();
 }
     
-function togglePasswordVisibility(inputId) {
-    const input = document.getElementById(inputId);
-    const type = input.getAttribute('type') === 'password' ? 'text' : 'password';
-    input.setAttribute('type', type);
-    
-    // Optionnel : Changer l'icône selon l'état
-    // event.target.innerText = type === 'password' ? '👁️' : '🙈';
-}
 }
 function togglePass(fieldId, icon) {
     const field = document.getElementById(fieldId);
